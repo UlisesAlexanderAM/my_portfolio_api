d6c8e3221491a9875cf83ac665d416dece6baed0
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2023-02-06 21:30:34 -0600

    docs: add doctrings to functions in skills.py

--- skills.py
+++ skills.py
@@ -18,8 +18,24 @@ class Skill:
 
 
 def obtain_name_stin(message: str) -> str:
+    """Function that ask for the name of the skill.
+
+    Args:
+        message (str): message show to the user
+
+    Returns:
+        str: name of the skill
+    """
     return input(message)
 
 
 def obtain_level_stin(message: str) -> str:
+    """Function that ask for the level of the skill
+
+    Args:
+        message (str): message shown to the user
+
+    Returns:
+        str: skill level
+    """
     return input(message)
