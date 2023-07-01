a378ba245fa05ae83ac44fbdc48e4e849b651dd0
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2023-06-30 19:56:12 -0600

    test: add test for adding skill and retrieving skill by name

--- app/tests/test_crud.py
+++ app/tests/test_crud.py
@@ -2,7 +2,7 @@ from fastapi import status
 from fastapi.testclient import TestClient
 
 from app import dependencies, main
-from app.config import test_database, database
+from app.config import database, test_database
 
 database.Base.metadata.create_all(bind=test_database.engine)
 
@@ -31,3 +31,17 @@ def test_get_skills():
     response = test_client.get("/skills")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == []
+
+
+def test_add_skill():
+    response = test_client.post("/skills", json={"name": "Python", "level": "0.5"})
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+def test_get_skill_by_name():
+    skill_name = "Python"
+    test_client.post("/skills", json={"name": "Python", "level": "0.5"})
+
+    response = test_client.get(f"/skills/{skill_name}")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"name": skill_name, "level": 0.5, "id": 1}
